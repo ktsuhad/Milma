7bd635c7c6c6d141352abecca48f9901273935ba
suhad <suhadkizhakethodi@gmail.com>
2023-11-19 08:01:18 +0530

    style: added styles

--- src/Components/Product/ProductItems.jsx
+++ src/Components/Product/ProductItems.jsx
@@ -4,7 +4,7 @@ const ProductItems = ({ bgColor, item }) => {
   return (
     <div
       style={{ borderWidth: "6px", borderStyle: "solid", borderColor: bgColor }}
-      className="w-full md:w-[382px]  rounded-6xl p-3"
+      className="w-full md:w-[382px]  rounded-6xl p-3 shadow-2xl"
     >
       <img src={item.image} alt={item.name} />
       <div className="flex justify-between py-3">
